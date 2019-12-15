dd9f8cc84467adf2228abe2879b8b43479e5f105
Elian Cordoba <eliancordoba98@gmail.com>
2019-12-15 17:21:46 -0300

    Show notes properly

--- pages/examples/[example].js
+++ pages/examples/[example].js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
+import { Divider, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import SideMenu from '../../src/components/SideMenu';
@@ -15,7 +16,7 @@ const useStyles = makeStyles(() => ({
 
 export default function ExamplePage() {
   const router = useRouter();
-  const { example } = router.query;
+  let { example } = router.query;
 
   const [notes, setNotes] = useState(null);
   const [examples, setExamples] = useState(null);
@@ -24,22 +25,21 @@ export default function ExamplePage() {
 
   useEffect(() => {
     async function getExamples() {
-      console.log('useEffect example')
+      console.log('useEffect example');
 
       // TODO: Sometimes 'example' is undefined
-      
+
       // const { notes, examples } = await import(`./${example}/code.js`);
       let notes = null;
       let examples = null;
 
-      console.log('exampoles es ', example)
+      console.log('exampoles es ', example);
 
-      if (!example) example= 'count'
+      if (!example) example = 'count';
 
       if (example) {
-        
         ({ notes, examples } = await import(`./${example}/code.js`));
-        console.log('AHHHH', examples)
+        console.log('AHHHH', examples);
       }
 
       setNotes(notes);
@@ -54,9 +54,18 @@ export default function ExamplePage() {
       <SideMenu></SideMenu>
       {examples && examples.length >= 2 && <Result data={examples}></Result>}
       <div className={classes.root}>
-        {examples && examples.map(code => <CodeBlock code={code} key={code.name}></CodeBlock>)}
-        <h1>{notes}</h1>
+        {examples &&
+          examples.map(code => (
+            <CodeBlock code={code} key={code.name}></CodeBlock>
+          ))}
       </div>
+      {notes && (
+        <div>
+          <Divider />
+          <Typography variant="h4">Notes</Typography>
+          <Typography variant="h6" dangerouslySetInnerHTML={{__html: notes }} />
+        </div>
+      )}
     </>
   );
 }
