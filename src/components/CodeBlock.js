93706257421bdf875dca9f9de794e44aafe57827
Elian Cordoba <eliancordoba98@gmail.com>
2019-12-15 17:10:13 -0300

    Update code block comp to new code format

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -14,40 +14,10 @@ const useStyles = makeStyles(() => ({
 export default function CodeBlock({ code }) {
   const classes = useStyles();
 
-  const parsedExample = parseCode(code);
-
-  const codeLength = parsedExample.code.split('').length;
-
   return (
     <div className={classes.root}>
-      <Typography variant="h6" align='center'>~{codeLength} characters</Typography>
-      <SyntaxHighlighter language="javascript">{parsedExample.code}</SyntaxHighlighter>
+      <Typography variant="h6" align='center'>~{code.length} characters</Typography>
+      <SyntaxHighlighter language="javascript">{code.code}</SyntaxHighlighter>
     </div>
   );
-}
-
-/**
- tal vez hacer que el object que se importa tenga un gettter asi es mas facil saber el length
- 
-  export default examples = [
-    {
-      name: 'svelte',
-      code: 'asdasdasd',
-      length: function() {
-        return this.code.split('').length
-      }
-    }
-  ]
-
-
- */
-
-// mover a util
-function parseCode(example) {
-  const name = Object.keys(example)[0];
-
-  return {
-    name,
-    code: example[name]
-  }
 }
