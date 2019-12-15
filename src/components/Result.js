f78ee2d2c88e32d28876633ccab30828acd0e260
Elian Cordoba <eliancordoba98@gmail.com>
2019-12-14 23:23:51 -0300

    Updated result comp to use new code format

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,46 +2,24 @@ import { useState, useEffect } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
-/**
- *
- * @param {*} data
- * Example: {
- *   react: '......',
- *   svelte: '...'
- * }
- */
 export default function Result({ data }) {
   const [shortest, setShortest] = useState(null);
   const [porcentage, setPorcentage] = useState(null);
-  let examples = [];
 
   useEffect(() => {
-    examples = data
-      .map(example => {
-        
-        /*
-          {
-            react: '......'
-          }
-        */
-
-        return {
-          ...example,
-          length: Object.values(example)[0].split('').length
-        };
-      })
-      .sort((a, b) => a.length - b.length);
-
-    const result = 100 - (examples[0].length / examples[1].length) * 100;
+    console.log('useEffect result')
+    const sortedArray = data.sort((a, b) => a.length - b.length);
+    
+    const result = 100 - (sortedArray[0].length / sortedArray[1].length) * 100;
 
     setPorcentage(result.toFixed(2));
-    debugger
-    // setShortest(capitalize(Object.keys(examples[0])[0]));
+    setShortest(sortedArray[0].name);
+
     // if (examples.length === 2) {
     // } else {
     //   throw new Error('TODO: Not implemented yet')
     // }
-  });
+  }, [data]);
 
   return (
     <Typography variant="h6" align="center">
