b4bea8abaa33d1109748980ec2c067a4333293b8
Elian Cordoba <eliancordoba98@gmail.com>
2019-12-16 16:52:18 -0300

    Updated identation in examples

--- src/examples/count/code.js
+++ src/examples/count/code.js
@@ -4,48 +4,47 @@ export const examples = [
   {
     name: 'React',
     code: 
-    `import React, { useState } from 'react';
-        
-    export default function Count() {
-      const [count, setCount] = useState(0);
+`import React, { useState } from 'react';
+    
+export default function Count() {
+  const [count, setCount] = useState(0);
 
-      const increment = () => setCount(count + 1);
-      const decrement = () => setCount(count - 1);
-      const reset = () => setCount(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
 
-      return (
-        <>
-          <h1>The count is {count}</h1>
+  return (
+    <>
+      <h1>The count is {count}</h1>
 
-          <button onClick={decrement}>-</button>
-          <button onClick={reset}>reset</button>
-          <button onClick={increment}>+</button>
-        </>
-      );
-    }`,
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={increment}>+</button>
+    </>
+  );
+}`,
     get length() {
       return getCodeLength(this);
     }
   },
   {
     name: 'Svelte',
-    code: `
-    <script>
-      import { writable } from "svelte/store";
-    
-      const count = writable(0);
-    
-      const increment = () => count.update(n => n + 1);
-      const decrement = () => count.update(n => n - 1);
-      const reset = () => count.set(0);
-    </script>
-    
-    <h1>The count is {$count}</h1>
-    
-    <button on:click={decrement}>-</button>
-    <button on:click={reset}>reset</button>
-    <button on:click={increment}>+</button>
-    `,
+    code: 
+`<script>
+  import { writable } from "svelte/store";
+
+  const count = writable(0);
+
+  const increment = () => count.update(n => n + 1);
+  const decrement = () => count.update(n => n - 1);
+  const reset = () => count.set(0);
+</script>
+
+<h1>The count is {$count}</h1>
+
+<button on:click={decrement}>-</button>
+<button on:click={reset}>reset</button>
+<button on:click={increment}>+</button>`,
     get length() {
       return getCodeLength(this);
     }
--- src/examples/todo-app/code.js
+++ src/examples/todo-app/code.js
@@ -3,122 +3,121 @@ import { getCodeLength } from '../../../src/utils';
 export const examples = [
   {
     name: 'React',
-    code: `
-    import React, { useState, useMemo } from 'react';
+    code: 
+`import React, { useState, useMemo } from 'react';
 
-    export default function TodoList() {
-      const [todos, setTodos] = useState([
-        { done: false, text: 'eat' },
-        { done: false, text: 'sleep' },
-        { done: false, text: 'code' },
-        { done: false, text: 'repeat' }
-      ]);
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { done: false, text: 'eat' },
+    { done: false, text: 'sleep' },
+    { done: false, text: 'code' },
+    { done: false, text: 'repeat' }
+  ]);
 
-      function toggleDone(t) {
-        setTodos(
-          todos.map(todo => {
-            if (todo === t) {
-              return {
-                done: !t.done,
-                text: t.text
-              };
-            }
-            return todo;
-          })
-        );
-      }
+  function toggleDone(t) {
+    setTodos(
+      todos.map(todo => {
+        if (todo === t) {
+          return {
+            done: !t.done,
+            text: t.text
+          };
+        }
+        return todo;
+      })
+    );
+  }
 
-      const [hideDone, setHideDone] = useState(false);
+  const [hideDone, setHideDone] = useState(false);
 
-      function toggleHideDone() {
-        setHideDone(!hideDone);
-      }
+  function toggleHideDone() {
+    setHideDone(!hideDone);
+  }
 
-      const filtered = useMemo(
-        () => (
-          hideDone 
-            ? todos.filter(todo => !todo.done) 
-            : todos
-          ),
-        [todos, hideDone]
-      );
+  const filtered = useMemo(
+    () => (
+      hideDone 
+        ? todos.filter(todo => !todo.done) 
+        : todos
+      ),
+    [todos, hideDone]
+  );
 
-      return (
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              checked={hideDone}
-              onChange={toggleHideDone}
-            ></input>
-            hide done
-          </label>
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={hideDone}
+          onChange={toggleHideDone}
+        ></input>
+        hide done
+      </label>
 
-          <ul>
-            {filtered.map((todo, index) => (
-              <li 
-                onClick={() => toggleDone(todo)} 
-                key={index}
-              >
-                {todo.done ? '👍' : ''} {todo.text}
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    }`,
+      <ul>
+        {filtered.map((todo, index) => (
+          <li 
+            onClick={() => toggleDone(todo)} 
+            key={index}
+          >
+            {todo.done ? '👍' : ''} {todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}`,
     get length() {
       return getCodeLength(this);
     }
   },
   {
     name: 'Svelte',
-    code: `
-    <script>
-      let todos = [
-        { done: false, text: 'eat' },
-        { done: false, text: 'sleep' },
-        { done: false, text: 'code' },
-        { done: false, text: 'repeat' }
-      ];
-    
-      function toggleDone(t) {
-        todos = todos.map(todo => {
-          if (todo === t) {
-            return { 
-              done: !t.done, 
-              text: t.text
-            };
-          }
-    
-          return todo;
-        });
+    code: 
+`<script>
+  let todos = [
+    { done: false, text: 'eat' },
+    { done: false, text: 'sleep' },
+    { done: false, text: 'code' },
+    { done: false, text: 'repeat' }
+  ];
+
+  function toggleDone(t) {
+    todos = todos.map(todo => {
+      if (todo === t) {
+        return { 
+          done: !t.done, 
+          text: t.text
+        };
       }
-    
-      let hideDone = false;
-    
-      $: filtered = hideDone
-        ? todos.filter(todo => !todo.done)
-        : todos;
-    </script>
-    
-    <label>
-      <input
-        type="checkbox"
-        bind:checked={hideDone}
-      >
-    
-      hide done
-    </label>
-    
-    <ul>
-      {#each filtered as todo}
-        <li on:click={() => toggleDone(todo)}>
-          {todo.done ? '👍' : ''} {todo.text}
-        </li>
-      {/each}
-    </ul>
-    `,
+
+      return todo;
+    });
+  }
+
+  let hideDone = false;
+
+  $: filtered = hideDone
+    ? todos.filter(todo => !todo.done)
+    : todos;
+</script>
+
+<label>
+  <input
+    type="checkbox"
+    bind:checked={hideDone}
+  >
+
+  hide done
+</label>
+
+<ul>
+  {#each filtered as todo}
+    <li on:click={() => toggleDone(todo)}>
+      {todo.done ? '👍' : ''} {todo.text}
+    </li>
+  {/each}
+</ul>`,
     get length() {
       return getCodeLength(this);
     }
