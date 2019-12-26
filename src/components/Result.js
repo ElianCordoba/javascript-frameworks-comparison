634dff5c7e7ef17d34f5d64b2c2567cb5ec5d9ab
Elian Cordoba <eliancordoba98@gmail.com>
2019-12-26 18:02:59 -0300

    Updated result component to display multiple comparisons

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,28 +1,55 @@
 import { useState, useEffect } from 'react';
 
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  item: {
+    fontWeight: 300,
+    textAlign: 'left',
+    marginLeft: '45%'
+  }
+}));
 
 export default function Result({ data }) {
-  const [shortest, setShortest] = useState(null);
-  const [porcentage, setPorcentage] = useState(null);
+  const classes = useStyles();
+
+  const [sortedArray, setSortedArray] = useState(null);
+  const [results, setResults] = useState(null);
 
   useEffect(() => {
-    const sortedArray = data.sort((a, b) => a.length - b.length);
-    
-    const result = 100 - (sortedArray[0].length / sortedArray[1].length) * 100;
+    const array = data.sort((a, b) => a.length - b.length);
 
-    setPorcentage(result.toFixed(2));
-    setShortest(sortedArray[0].name);
+    let results = [];
+    array.map((example, index) => {
+      // Omit first element since we don't want to compare it with itself
+      if (index === 0) return;
 
-    // if (examples.length === 2) {
-    // } else {
-    //   throw new Error('TODO: Not implemented yet')
-    // }
+      const porcentage = 100 - (array[0].length / example.length) * 100;
+      results.push(porcentage.toFixed(2));
+    });
+
+    setResults(results);
+    setSortedArray(array);
   }, [data]);
 
   return (
     <Typography variant="h6" align="center">
-      {shortest} is {porcentage}% smaller
+      {sortedArray && sortedArray[0].name} code is the shortest, comperad to
+      other options it's:
+      {results &&
+        // We need to add one to the index in order to ommit the shortest one
+        results.map((x, index) =>
+          x < 0.5 ? (
+            <li key={index} className={classes.item}>
+              Basically the same compared to {sortedArray[index + 1].name}
+            </li>
+          ) : (
+            <li key={index} className={classes.item}>
+              {x}% shorter than {sortedArray[index + 1].name}
+            </li>
+          )
+        )}
     </Typography>
   );
 }
